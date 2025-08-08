a28e5194118fc53cf9cd2926828e06197b5b7798
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-08-07 23:59:19 -0300

    feat: sidebar data

--- src/components/ReusableComponent/LayoutComponents/Sidebar.tsx
+++ src/components/ReusableComponent/LayoutComponents/Sidebar.tsx
@@ -1,5 +1,13 @@
+"use client";
 import { usePathname } from "next/navigation";
 import React from "react";
+import { sidebarData } from "@/lib/data";
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import Link from "next/link";
 
 type Props = {};
 
@@ -7,7 +15,35 @@ const Sidebar = (props: Props) => {
   const pathname = usePathname();
 
   return (
-    <div className="w-18 sm:w-28 h-screen sticky top-0 py-10 px-2 sm:px-6 border bg-background border-border flex flex-col items-center justify-center gap-10"></div>
+    <div className="w-18 sm:w-28 h-screen sticky top-0 py-10 px-2 sm:px-6 border bg-background border-border flex flex-col items-center justify-center gap-10">
+      <div className="">=</div>
+      <div className="w-full h-full justify-between items-center flex flex-col">
+        <div className="w-full h-fit flex flex-col gap-4 items-center justify-center">
+          {sidebarData.map((item) => (
+            <TooltipProvider key={item.id}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Link
+                    href={item.link}
+                    className={`flex items-center gap-2 cursor-pointer rounded-lg p-2
+                    ${pathname.includes(item.link) ? "iconBackground" : ""}
+                    `}
+                  >
+                    <div
+                      className={`${
+                        pathname.includes(item.link) ? "" : "opacity-80"
+                      }`}
+                    >
+                      {item.icon}
+                    </div>
+                  </Link>
+                </TooltipTrigger>
+              </Tooltip>
+            </TooltipProvider>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };
 
---
+++ src/lib/data.ts
@@ -0,0 +1,32 @@
+export const sidebarData = [
+  {
+    id: 1,
+    title: "Home",
+    icon: "home",
+    link: "/home",
+  },
+  {
+    id: 2,
+    title: "Webinars",
+    icon: "webinar",
+    link: "/webinars",
+  },
+  {
+    id: 3,
+    title: "Leads",
+    icon: "leads",
+    link: "/leads",
+  },
+  {
+    id: 4,
+    title: "AI Agents",
+    icon: "ai-agent",
+    link: "/ai-agents",
+  },
+  {
+    id: 5,
+    title: "Settings",
+    icon: "settings",
+    link: "/settings",
+  },
+];
