693b746b5588075b90543ebd68d7f1be45559226
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-08-27 23:20:57 -0300

    feat: BasicInfoStep description

--- src/components/ReusableComponent/CreateWebinarButton/BasicInfoStep.tsx
+++ src/components/ReusableComponent/CreateWebinarButton/BasicInfoStep.tsx
@@ -1,3 +1,4 @@
+import { Textarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
 import { useWebinarStore, useWebinarStore } from "@/store/useWebinarStore";
 import React from "react";
@@ -5,18 +6,28 @@ import React from "react";
 type Props = {};
 
 const BasicInfoStep = (props: Props) => {
-  const { formData, updateBasicField, getStepValidationErrors } =
+  const { formData, updateBasicInfoField, getStepValidationErrors } =
     useWebinarStore();
+  const { webinarName, description, date, time, timeFormat } =
+    formData.basicInfo;
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    updateBasicInfoField(name as keyof typeof formData.basicInfo, value);
+  };
 
   const errors = getStepValidationErrors("basicInfo");
+
   return (
     <div className="space-y-6">
       <div className="space-y-2">
         <label
           htmlFor="webinarName"
-          className={errors.webinarName ? "text-red-500" : ""}
+          className={errors.webinarName ? "text-red-400" : ""}
         >
-          Webinar name. <span className="text-red-500">*</span>
+          Webinar name. <span className="text-red-400">*</span>
         </label>
         <input
           id="webinarName"
@@ -31,6 +42,28 @@ const BasicInfoStep = (props: Props) => {
         />
         {errors.webinarName && (
           <p className="text-sm text-red-400">{errors.webinarName}</p>
+        )}
+      </div>
+      <div className="space-y-2">
+        <label
+          htmlFor="description"
+          className={errors.description ? "text-red-400" : ""}
+        >
+          Description. <span className="text-red-400">*</span>
+        </label>
+        <Textarea
+          id="description"
+          name="description"
+          value={description || ""}
+          onChange={handleChange}
+          placeholder="Tell customers what your webinar is about"
+          className={cn(
+            "min-h-[100px] !bg-background/50 border border-input",
+            errors.description && "border-red-400 focus-visible:ring-red-400"
+          )}
+        />
+        {errors.description && (
+          <p className="text-sm text-red-400">{errors.description}</p>
         )}
       </div>
     </div>
