5378e73ba04da20a75a8988b52575451e4fc3de6
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-09-12 23:35:13 -0300

    feat: lead page

---
+++ src/app/(protectedRoutes)/lead/__tests__/data.ts
@@ -0,0 +1,68 @@
+export const leadData = [
+  {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "+1234567890",
+    tags: ["New Customer", "Tag1", "Tag2"],
+  },
+  {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "+1234567890",
+    tags: ["New Customer", "Tag1", "Tag2"],
+  },
+  {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "+1234567890",
+    tags: ["New Customer", "Tag1", "Tag2"],
+  },
+  {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "+1234567890",
+    tags: ["New Customer", "Tag1", "Tag2"],
+  },
+  {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "+1234567890",
+    tags: ["New Customer", "Tag1", "Tag2"],
+  },
+  {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "+1234567890",
+    tags: ["New Customer", "Tag1", "Tag2"],
+  },
+  {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "+1234567890",
+    tags: ["New Customer", "Tag1", "Tag2"],
+  },
+  {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "+1234567890",
+    tags: ["New Customer", "Tag1", "Tag2"],
+  },
+  {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "+1234567890",
+    tags: ["New Customer", "Tag1", "Tag2"],
+  },
+  {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "+1234567890",
+    tags: ["New Customer", "Tag1", "Tag2"],
+  },
+  {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "+1234567890",
+    tags: ["New Customer", "Tag1", "Tag2"],
+  },
+];
