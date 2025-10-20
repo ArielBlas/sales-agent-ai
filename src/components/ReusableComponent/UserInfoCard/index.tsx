911cd717533ed2ee7f4e69a9a68bf681f81ea265
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-10-20 18:42:48 -0300

    feat: UserInfoCard changes

--- src/components/ReusableComponent/UserInfoCard/index.tsx
+++ src/components/ReusableComponent/UserInfoCard/index.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { Attende } from "@prisma/client";
+import { format } from "date-fns";
 import React from "react";
 
 type Props = {
@@ -12,22 +13,28 @@ const UserInfoCard = ({ customer, tags, className }: Props) => {
   return (
     <div
       className={cn(
-        "flex flex-col w-fit text-primary p-3 pr-10 gap-3 rounded-xl border-[0.5px] border-border backdrop-blur-[20px] bg-background/10",
+        `flex flex-col w-fit text-primary p-3 pr-10 gap-3 rounded-xl border-[0.5px] 
+        border-border backdrop-blur-[20px] bg-secondary`,
         className
       )}
     >
-      <h3 className="font-semibold text-xs">{customer.name}</h3>
-      <p className="text-sm">{customer.email}</p>
+      <h3 className="font-semibold text-xs text-primary">{customer.name}</h3>
+      <p className="text-sm text-muted-foreground">{customer.email}</p>
       <div className="flex gap-2 flex-wrap">
         {tags.map((tag, index) => (
           <span
             key={index}
-            className="text-foreground px-3 py-1 rounded-md border border-border"
+            className="text-foreground px-3 py-1 rounded-md border border-border
+            text-sm bg-background
+            "
           >
             {tag}
           </span>
         ))}
       </div>
+      <p className="text-xs font-normal text-muted-foreground">
+        Updated At: {format(customer?.updatedAt, "MMM d, yyyy")}
+      </p>
     </div>
   );
 };
