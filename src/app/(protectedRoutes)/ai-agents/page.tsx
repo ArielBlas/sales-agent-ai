1892668f0c1d339f78d64fd9571bf00772316b6e
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-11-19 18:55:14 -0300

    feat: ModelSection component

--- src/app/(protectedRoutes)/ai-agents/page.tsx
+++ src/app/(protectedRoutes)/ai-agents/page.tsx
@@ -1,6 +1,7 @@
 import { getAllAssistants } from "@/actions/vapi";
 import React from "react";
 import AiAgentSidebar from "./_components/AiAgentSidebar";
+import ModelSection from "./_components/ModelSection";
 
 type Props = {};
 
@@ -9,7 +10,9 @@ const page = async (props: Props) => {
   return (
     <div className="w-full flex h-[80vh] text-primary border border-border rounded-se-xl">
       <AiAgentSidebar aiAgents={allAgents?.data || []} />
-      <div className="flex-1 flex flex-col"></div>
+      <div className="flex-1 flex flex-col">
+        <ModelSection />
+      </div>
     </div>
   );
 };
